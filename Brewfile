721ea64f2ed9d9887c995219bf097005fc49f859
Jeff Day <deffjay@gmail.com>
2019-07-24 18:30:40 -0700

    - Update bash to 5.x version, auto complete is still quite buggy - upgraded to autocomplete@2

--- Brewfile
+++ Brewfile
@@ -1,31 +1,15 @@
+tap "caskroom/versions" 
+cask "adoptopenjdk8" #Open bug https://github.com/AdoptOpenJDK/homebrew-openjdk/issues/106
+
 tap "homebrew/cask"
-brew "mas"
-mas "Xcode", id: 497799835
-cask 'visual-studio-code'
-cask "android-studio"
-brew "screenfetch"
-brew "git"
-brew "git-lfs"
-brew "lynx"
-brew "p7zip"
-brew "pigz"
-brew "pv"
-brew "rename"
-brew "rlwrap"
-brew "ack"
-brew "ssh-copy-id"
-brew "tree"
-brew "vbindiff"
-brew "zopfli"
-brew "openssh"
-brew "openssl"
-brew "tmux"
-brew "grep"
-brew "php"
-brew "gmp"
+mas 'xcode', id: 497799835 
+mas '1Password', id: 1333542190
+
+cask "visual-studio-code"
 cask "spectacle"
 cask "karabiner-elements"
 cask "sourcetree"
-cask "skype"
-cask "slack"
-cask "java8"
+cask 'docker'
+cask 'dropbox'
+cask 'virtualbox'
+cask 'iterm2'
--- brew.sh
+++ brew.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# Install command-line tools using Homebrew.
-
 # Make sure we’re using the latest Homebrew.
 brew update
 
@@ -11,26 +9,36 @@ brew upgrade
 # Save Homebrew’s installed location.
 BREW_PREFIX=$(brew --prefix)
 
-# Install GNU core utilities (those that come with macOS are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
-
-# Install some other useful utilities like `sponge`.
-brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed
-# Install Bash 4.
-brew install bash
-brew install bash-completion2
-
 # Switch to using brew-installed bash as default shell
 if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
   echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
   chsh -s "${BREW_PREFIX}/bin/bash";
 fi;
+
+#Install a bunch of Jeffs binaries and MacOS applications
+brew "bundle"
+
+echo "Install everything in the Brewfile"
+# Upgrading to 5.0 from Mac default 3.2
+brew install bash
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils 
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed
+
+brew install git
+
+# Install Bash5 completion
+brew install bash-completion2
 
 # Install `wget`
 brew install wget
@@ -38,14 +46,38 @@ brew install wget
 # Install GnuPG to enable PGP-signing commits.
 brew install gnupg
 
-# Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
-
 # Install other useful binaries.
 brew install ack
 
-#Install a bunch of Jeffs binaries and MacOS applications
-brew bundle
+# Install more recent versions of some macOS tools.
+brew install vim
 
-# Remove outdated versions from the cellar.
+brew install cleanup
+brew install screenfetch
+brew install git-lfs
+brew install lynx
+brew install p7zip
+brew install pigz
+brew install pv
+brew install rename
+brew install rlwrap
+brew install ack
+brew install 'ssh-copy-id'
+brew install tree
+brew install vbindiff
+brew install zopfli
+brew install openssh
+brew install openssl
+brew install tmux
+brew install grep
+brew install php
+brew install gmp
+
+# Manually download the git-completions and git-prompt file
+wget -q -P /usr/local/etc/profile.d https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+wget -q -P /usr/local/etc/profile.d https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+
+echo "Remove outdated versions from the cellar"
 brew cleanup
